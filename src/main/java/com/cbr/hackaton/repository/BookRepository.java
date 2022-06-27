f85cc14d2e739a1e4682a61d008d0ed24b6aba8d
Александр Коваленко <stukova.katya@mail.ru>
2022-06-27 16:20:38 +0500

    feat: add books to data rest

--- src/main/java/com/cbr/hackaton/repository/BookRepository.java
+++ src/main/java/com/cbr/hackaton/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.cbr.hackaton.repository;
 
 import com.cbr.hackaton.entity.Book;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book, Long> {
 }
