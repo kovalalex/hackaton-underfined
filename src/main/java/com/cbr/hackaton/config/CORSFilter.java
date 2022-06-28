208d51414601d5f11ca0143619966b3f8900f671
Александр Коваленко <stukova.katya@mail.ru>
2022-06-28 13:05:50 +0500

    fix: add PATCH to cors

--- src/main/java/com/cbr/hackaton/config/CORSFilter.java
+++ src/main/java/com/cbr/hackaton/config/CORSFilter.java
@@ -17,7 +17,7 @@ public class CORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws ServletException, IOException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, HEAD");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, HEAD, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         chain.doFilter(req, res);
